e21fd7dcca2a5f96f93a4eb00f013e8581743f8c
Gio Khvedelidze <giorgi.khvedelidze.1@btu.edu.ge>
2023-06-18 21:16:02 +0400

    First Commit

---
+++ Controllers/OwnerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OwnerController : Controller
+{
+    private readonly IOwnerRepository _ownerRepository;
+
+    public OwnerController(IOwnerRepository ownerRepository)
+    {
+        _ownerRepository = ownerRepository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+    public IActionResult GetOwners()
+    {
+        var owners = _ownerRepository;
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(owners);
+    }
+}
---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class OwnerRepository : IOwnerRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public OwnerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public ICollection<Owner> GetOwners()
+    {
+        return _context.Owners.ToList();
+    }
+}
